23e2beb35250f81a0a9a3741252d434023a240be
moff <mswelatumelo@gmail.com>
2024-10-03 14:43:51 +0200

    Initial Commit

---
+++ src/main/java/com/api/CyberSentry/CyberSentryApplication.java
@@ -0,0 +1,13 @@
+package com.api.CyberSentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberSentryApplication.class, args);
+	}
+
+}
