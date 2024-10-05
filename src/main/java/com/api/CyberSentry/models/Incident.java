fdad75f8fc8213cb142cbd0756e872dca345400a
moff <mswelatumelo@gmail.com>
2024-10-05 21:08:12 +0200

    created incident entity

---
+++ src/main/java/com/api/CyberSentry/models/Incident.java
@@ -0,0 +1,13 @@
+package com.api.CyberSentry.models;
+
+public class Incident {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String priority;
+//    private IncidentStatus status;
+
+    private User assignee;
+
+}
