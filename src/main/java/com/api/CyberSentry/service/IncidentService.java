ce229ae256bdc29d60c4ada08c126095be76bf07
moff <mswelatumelo@gmail.com>
2024-10-07 11:21:13 +0200

    added more CRUD operations logic

--- src/main/java/com/api/CyberSentry/service/IncidentService.java
+++ src/main/java/com/api/CyberSentry/service/IncidentService.java
@@ -7,6 +7,9 @@ import com.api.CyberSentry.repository.UserRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class IncidentService {
 
@@ -35,5 +38,61 @@ public class IncidentService {
     }
 
     //Get all incidents
-    
+    public List<Incident> getAllIncidents(){
+        try {
+            return incidentRepository.findAll();
+        }
+        catch (Exception e){
+            throw new RuntimeException("Cannot retrieve all incidents " + e.getMessage());
+        }
+    }
+
+    //Get a specific incident
+    public Optional<Incident> getIncidentByTitle(String title){
+        try{
+            return getAllIncidents()
+                    .stream()
+                    .filter(Incident -> title.equals(Incident.getTitle()))
+                    .findFirst();
+        }
+        catch (Exception e){
+            throw new RuntimeException("Incident " + e.getMessage() + "not found");
+        }
+    }
+
+    //Update incident
+    public Optional<Incident> updateIncident(Long id, Incident incidentBeingUpdated){
+        try{
+            Optional<Incident> existingIncident = incidentRepository.findById(id);
+            if (existingIncident.isPresent()){
+                Incident existingIncident1 = existingIncident.get();
+                existingIncident1.setTitle(incidentBeingUpdated.getTitle());
+                existingIncident1.setDescription(incidentBeingUpdated.getDescription());
+                existingIncident1.setAffectedSystems(incidentBeingUpdated.getAffectedSystems());
+                existingIncident1.setPriority(incidentBeingUpdated.getPriority());
+                existingIncident1.setStatus(incidentBeingUpdated.getStatus());
+                existingIncident1.setAssignee(incidentBeingUpdated.getAssignee());
+
+                Incident savedIncident = incidentRepository.save(incidentBeingUpdated);
+                return Optional.of(savedIncident);
+            }
+            else {
+                return Optional.empty();
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException("Failed to update incident " + e.getMessage());
+        }
+    }
+
+    //Delete Incident
+    public boolean deleteIncident(Long id){
+        try {
+            incidentRepository.deleteById(id);
+            return true; //if successul
+        }
+        catch (Exception e){
+            throw new RuntimeException("Failed to delete incident " + e.getMessage() + id);
+        }
+    }
 }
