bf2ebe693baa339a74c131a8dbf78ba9c3882a83
moff <mswelatumelo@gmail.com>
2024-10-15 11:09:29 +0200

    added constructor with field Id

--- src/main/java/com/api/CyberSentry/models/User.java
+++ src/main/java/com/api/CyberSentry/models/User.java
@@ -27,17 +27,17 @@ public class User {
     public User() {
     }
 
-    public User(String email, String username, String password) {
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
-
     public User(Long id,
                 String email,
                 String username,
                 String password) {
         this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String email, String username, String password) {
         this.email = email;
         this.username = username;
         this.password = password;
