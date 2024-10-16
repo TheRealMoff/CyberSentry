a9371512a13bd26296176fd491984c6f99e52bb5
moff <mswelatumelo@gmail.com>
2024-10-16 09:39:19 +0200

    added methods to find by status, priority and username

--- src/main/java/com/api/CyberSentry/repository/IncidentRepository.java
+++ src/main/java/com/api/CyberSentry/repository/IncidentRepository.java
@@ -3,5 +3,11 @@ package com.api.CyberSentry.repository;
 import com.api.CyberSentry.models.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
+
+    List<Incident> findByPriority(String priority);
+    List<Incident> findByStatus(String status);
+    List<Incident> findByAssigneeUsername(String username);
 }
